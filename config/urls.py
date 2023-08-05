526a46778f7835ebeb1af50b32c3161ca570a25c
Rashid Doniyorov <rashiddoniyorov05@gmail.com>
2023-08-05 16:22:26 +0500

    deployment

---
+++ config/urls.py
@@ -0,0 +1,10 @@
+from django.contrib import admin
+from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
+
+urlpatterns = [
+    path('accounts/', include('allauth.urls')),
+    path("admin/", admin.site.urls),
+    path('', include('todo.urls'))
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
